0d638547da9556e010653cad07537d4743b22ec8
joshstevens19 <joshstevens19@hotmail.co.uk>
2024-05-15 10:00:00 +0000

    feat: export evaluation modules

---
+++ stages/betting/index.ts
@@ -0,0 +1,5 @@
+export { BaseBettingStage, saveBettor, loadBettor } from "./baseStage.js";
+export { ClassifierStage } from "./classifierStage.js";
+export { OddsCompareStage } from "./oddsCompareStage.js";
+export { runBacktestStage } from "./backtestStage.js";
+export type { BacktestRow } from "./backtestStage.js";
