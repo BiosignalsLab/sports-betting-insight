b71e2a5378e619be6282d8d36658b4543147fcde
joshstevens19 <joshstevens19@hotmail.co.uk>
2024-12-15 10:00:00 +0000

    feat: export dataset loaders

---
+++ adapters/data/index.ts
@@ -0,0 +1,3 @@
+export { BaseDataAdapter, loadDataLoader } from "./baseAdapter.js";
+export { MockSoccerAdapter } from "./mockSoccerAdapter.js";
+export { LiveSoccerAdapter } from "./liveSoccerAdapter.js";
